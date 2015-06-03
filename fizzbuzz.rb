0166090034cf4cb2cd1c648b17c57c7744c1ac00
John O'Brien <JohnHeywardOBrien@users.noreply.github.com>
2015-06-03 09:26:23 -0500

    Removed clutter, added new solution
    Old solution was not DRY. New solution fulfills a few new requirements as well.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,11 @@
 #!/usr/bin/env ruby
 
-puts "Here we shall be doing some Fizzing and some Buzzing. Onwards!"
-puts "Let's start with the basics:"
+  # This was the first solution I came up with
+  # While this works, it is NOT DRY and is a bit hard to read
+  # It is also not flexible, so if I had to change the numbers, I'd have to
+  # do it in several places
+puts "This solution fulfills the requirments of FizzBuzz, but is NOT D.R.Y."  
 
-  # && is the same as and, so we will use it here
-  # % is our modulus operator and divides the left hand operator by the right
-  
-  # our goal is to have multiples of 3 say Fizz
-  # multiples of 5 will say Buzz
-  # and numbers that are multiples of 3 AND 5, print FizzBuzz
-  
-  # here we set our range, and then we will iterate through it and use
-  # if, elsif and else to set our conditions
-  
 1.upto(100) do |i| 
   if i % 3 == 0 && i % 5 == 0
     puts 'FizzBuzz'
@@ -23,4 +16,21 @@ puts "Let's start with the basics:"
   else
     puts i
   end
+end
+
+  # The folloowing solution was created after being asked to 
+  # make the output have the number followed by a . and space
+  # and to be on a new line for each number. 
+puts "This solution puts all numbers on a new line followed by a . and a space."
+puts "It is more D.R.Y. than the previous exmpale and could easily be modified to use different numbers"
+
+1.upto(100) do |i|
+  print i.to_s + '. '
+  if i % 3 == 0
+    print 'Fizz'
+  end
+  if i % 5 == 0
+    print 'Buzz'
+  end
+  puts
 end
